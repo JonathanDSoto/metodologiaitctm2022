4080695ab9d673efa310ac58d1c919dcb851558a
Jonathan Soto <jonathansoto@MacBook-Pro-de-Jonathan.local>
2022-09-14 10:18:30 -0600

    area triangulo

---
+++ area_de_triangulo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 07:04:32 2022
+
+@author: jonathansoto
+"""
+
+#declaración 
+base = 0
+altura = 0
+resultado = 0
+
+#lectura de datos
+base = int( input("Ingrese la base \n") )
+altura = int( input( "Ingrese la altura \n" ) )
+
+#operaciones
+resultado = base * altura / 2
+
+print("El área del triangulo es: ", resultado)
+    
+    
+    
